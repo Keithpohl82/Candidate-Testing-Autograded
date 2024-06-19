4084138d281bc1435269b8af5a824a831066c22e
keithpohl82 <keith.pohl82@gmail.com>
2024-06-19 10:41:07 -0500

    without line 65

--- candidate-testing.js
+++ candidate-testing.js
@@ -62,7 +62,7 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
-runProgram();
+
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
