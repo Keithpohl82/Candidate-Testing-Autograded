25e297cdff25ce17c84eb20727298109e4f52b69
keithpohl82 <keith.pohl82@gmail.com>
2024-06-19 10:19:32 -0500

    Testing spacing for autograder

--- candidate-testing.js
+++ candidate-testing.js
@@ -20,8 +20,9 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your name? ");
 
+
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
@@ -58,7 +59,7 @@ function runProgram() {
   // TODO 1.1c: Greet candidate using their name //
    console.log("Welcome " + candidateName);
   askQuestion();
-  gradeQuiz(this.candidateAnswers.toLowerCase());
+  gradeQuiz(this.candidateAnswers);
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
